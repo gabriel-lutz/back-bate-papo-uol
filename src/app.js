e87f6e0c9c6fed4a80d6d83d07100232c37af915
Gabriel Peres Lutz <gabriel.p.lutz@gmail.com>
2021-06-10 17:42:48 -0300

    adicionado o metodo post para a rota /participants adicionando o novo participante na memoria

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import express from "express"
+import cors from "cors"
+import dayjs from "dayjs"
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const participants = [{}]
+
+const messages = [{}]
+
+app.post("/participants", (req,res)=>{
+    const newParticipant = {...req.body, lastStatus: Date.now()}
+    if(newParticipant.name.length === 0 || participants.some(p=> p.name === newParticipant.name)){
+        res.send(400)
+    }else{
+        participants.push(newParticipant)
+        messages.push({
+            from: newParticipant.name,
+            to: "Todos",
+            text: "entra na sala ...",
+            type: "status",
+            time: dayjs().format('HH:MM:ss')
+        })
+        res.sendStatus(200)
+    }
+})
+
+app.listen(4000, ()=>{
+    console.log("O servidor está rodando na porta 4000 ...")
+})
